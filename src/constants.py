0fa23d17c12c219e292c3d1c1ba87f0a0a18cafa
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-10-04 22:16:13 +1000

    add constants.py

---
+++ src/constants.py
@@ -0,0 +1,26 @@
+# pin 0 free
+# pin 1 free
+PinEAWinch = 2
+PinEBWinch = 3
+PinForwardLimit = 4
+PinReverseLimit = 5
+PinTRIGleft = 6
+PinECHOleft = 7
+PinTRIGright = 8
+PinECHOright = 9
+PinENWinch = 10
+PinIN1winch = 11
+PinIN2winch = 12
+PinENtraversal = 13
+PinIN1traversal = 14
+PinIN2traversal = 15
+PinDIR = 16
+PinSTEP = 17
+PinSLEEP_inverted = 18
+PinIN2arm = 19
+PinIN1arm = 20
+PinENarm = 21
+PinServo = 22
+PinEBarm = 26
+PinEAarm = 27
+PinInternalLED = 25
